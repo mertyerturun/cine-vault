9a6a51f81dfedd03ce0e80286ac08acec1f06823
Mert Yertürün <mertyerturun@hotmail.com>
2026-02-15 21:45:22 +0300

    gereksiz import silindi

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import type { Movie } from '../Interfaces/Movie';
 import { AddMovieForm } from '../Components/AddMovieForm';
 import { MovieItem } from '../Components/MovieItem';
